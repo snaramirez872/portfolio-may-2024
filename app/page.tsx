7f02bcf6def71069410f58daaf295b9f21f14428
snaramirez872 <seanramirez818@gmail.com>
2024-05-31 14:44:43 -0400

    setting up for about me section implementation later

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,24 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
-      <Link href='/projects'>Projects</Link>
+      <section className="About-Me">
+        <h2>About Me</h2>
+        <div className='link-icons'>
+          <a href='#' target='_blank' rel='noreferrer'></a>
+          <a href='#' target='_blank' rel='noreferrer'></a>
+          <a href='#' target='_blank' rel='noreferrer'></a>
+          <a href='#' target='_blank' rel='noreferrer'></a>
+          <a href='#' target='_blank' rel='noreferrer'></a>
+        </div>
+        <div className='about-content'>
+          <p>[Replace with Text]</p>
+          <p>[Replace with Text]</p>
+        </div>
+      </section>
+      <div>
+        <Link href='/projects' className="block md:hidden">Projects</Link>
+        <Link href='#' className="block md:hidden">Resume</Link>
+      </div>
     </main>
   );
 }
