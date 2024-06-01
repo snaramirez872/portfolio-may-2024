edb1b37b0282aabb84770560521f6cb082e9ba95
snaramirez872 <seanramirez818@gmail.com>
2024-06-01 10:55:32 -0400

    fixed nav bar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,15 +1,16 @@
 'use client';
-import React, { useState } from 'react'
+import React from 'react'
 import Link from 'next/link';
 
 export const NavBar = () => {
     return (
-        <nav className="bg-slate-900 text-white p-5 md:justify-between md:items-center">
-            <div className="container mx-auto flex justify-between items-center">
-                <a href="/" className="text-3xl font-bold">Sean A Ramirez</a>
-                <div className='space-x-9 text-xl hidden md:flex'>
-                    <Link href="/projects" className='mx-2 p-3 hover:text-emerald-300'>Projects</Link>
-                    <Link href="#" className='md:bg-orange-500 md:rounded-xl lg:hover:bg-orange-600 lg:hover:rounded-3xl p-3 mx-2' target='_blank' rel='noreferrer'>Resume</Link>
+        <nav className="text-white p-5 w-screen">
+            <div className="flex justify-between items-center w-full">
+                <a href="/" className="ml-[7vh] text-4xl font-bold">Sean A Ramirez</a>
+                <div className='mr-[7vh] space-x-11 text-xl hidden md:flex'>
+                    <Link href="/" className='p-3 hover:text-emerald-300'>About Me</Link>
+                    <Link href="/projects" className='p-3 hover:text-emerald-300'>Projects</Link>
+                    <Link href="#" className='md:bg-emerald-300 md:text-black md:rounded-xl md:hover:bg-emerald-500 lg:hover:rounded-3xl p-3' target='_blank' rel='noreferrer'>Resume</Link>
                 </div>
             </div>
         </nav>
