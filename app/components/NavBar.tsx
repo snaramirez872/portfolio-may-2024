aecc9f374dae449e187e6cb0c471ca8aa6710aef
snaramirez872 <seanramirez818@gmail.com>
2024-06-01 13:05:46 -0400

    implemented and designed landing page for both desktop and mobile

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -10,7 +10,7 @@ export const NavBar = () => {
                 <div className='mr-[7vh] space-x-11 text-xl hidden md:flex'>
                     <Link href="/" className='p-3 hover:text-emerald-300'>About Me</Link>
                     <Link href="/projects" className='p-3 hover:text-emerald-300'>Projects</Link>
-                    <Link href="#" className='md:bg-emerald-300 md:text-black md:rounded-xl md:hover:bg-emerald-500 lg:hover:rounded-3xl p-3' target='_blank' rel='noreferrer'>Resume</Link>
+                    <Link href="https://drive.google.com/file/d/1xt08p77uQfUoizwI1NsAV8rXP19l-zTV/view" className='md:bg-emerald-300 md:text-black md:rounded-xl lg:hover:rounded-3xl p-3' target='_blank' rel='noreferrer'>Resume</Link>
                 </div>
             </div>
         </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,45 @@
+import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import ProfilePic from '../public/icons/profile.png';
+import email from '../public/icons/envelope.svg';
+import github from '../public/icons/github-mark-white.png';
+import linkedin from '../public/icons/In-White-96@2x.png';
+import x from '../public/icons/x-logo-white.png';
+import "./components/About.css";
+
 
 export default function Home() {
   return (
     <main>
       <section className="About-Me">
-        <h2>About Me</h2>
-        <div className='link-icons'>
-          <a href='#' target='_blank' rel='noreferrer'></a>
-          <a href='#' target='_blank' rel='noreferrer'></a>
-          <a href='#' target='_blank' rel='noreferrer'></a>
-          <a href='#' target='_blank' rel='noreferrer'></a>
-          <a href='#' target='_blank' rel='noreferrer'></a>
-        </div>
-        <div className='about-content'>
-          <p>[Replace with Text]</p>
-          <p>[Replace with Text]</p>
+        <div id='about-content' className='pl-5'>
+          <div className='words'>
+            <p>I am a <span className='text-[22px] font-semibold text-orange-500'>Software Developer</span> who recently graduated from the New Jersey Institute of Technology. My focuses lie primarily in <span className='text-[22px] font-semibold text-orange-500'>Web Development</span> and general <span className='text-[22px] font-semibold text-orange-500'>Front End Development/Design</span>. As such, I am proficient in <span className='text-[22px] font-semibold text-orange-500'>JavaScript</span> and <span className='text-[22px] font-semibold text-orange-500'>Python</span>.</p>
+            <p className='mt-6'>When I am not coding, I enjoy playing video games, photography, listening to music, and anime.</p>
+            <p className='mt-6'>I've been making <span className='text-[22px] font-semibold text-orange-500'>React.js</span> applications for several months now and they all can be viewed by <span className='hidden md:inline'>clicking "Projects" at the top right</span><span className='inline md:hidden'>tapping "Projects" below</span>.</p>
+          </div>
+          <div className='profile-card'>
+            <Image className='profile-picture' src={ProfilePic} alt="profile picture" />
+            <div className='flex gap-8 mt-5'>
+              <Link href='mailto:snaramirez872@gmail.com' target='_blank' rel='noreferrer'><Image className='icon' src={email} alt="email" /></Link>
+              <Link href='https://github.com/snaramirez872' target='_blank' rel='noreferrer'><Image className='icon' src={github} alt="github" /></Link>
+              <Link href='https://linkedin.com/in/seanaramirez' target='_blank' rel='noreferrer'><Image className='icon' src={linkedin} alt="linkedin" /></Link>
+              <Link href='https://x.com/snaramirez' target='_blank' rel='noreferrer'><Image className='icon' src={x} alt="x" /></Link>
+            </div>
+          </div>
         </div>
       </section>
-      <div>
-        <Link href='/projects' className="block md:hidden">Projects</Link>
-        <Link href='#' className="block md:hidden">Resume</Link>
+      <div className='mobile-tabs'>
+        <Link href='/projects' className="">Projects</Link>
+        <Link href='#' className="">Resume</Link>
       </div>
+      <div id='link-icons' className='md:hidden ml-[7vh] pl-5 flex gap-[6vh]'>
+          <Link href='mailto:snaramirez872@gmail.com' target='_blank' rel='noreferrer'><Image className='icon' src={email} alt="email" /></Link>
+          <Link href='https://github.com/snaramirez872' target='_blank' rel='noreferrer'><Image className='icon' src={github} alt="github" /></Link>
+          <Link href='https://linkedin.com/in/seanaramirez' target='_blank' rel='noreferrer'><Image className='icon' src={linkedin} alt="linkedin" /></Link>
+          <Link href='https://x.com/snaramirez' target='_blank' rel='noreferrer'><Image className='icon' src={x} alt="x" /></Link>
+        </div>
     </main>
   );
 }
